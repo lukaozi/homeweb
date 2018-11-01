10d54666937eb2fa42f757133e3588626aa762b0
lucas2 <lukaozi@163.com>
2018-11-01 15:13:43 +0800

    首次创建

--- src/main/java/com/index/home/facade/HelloController.java
+++ src/main/java/com/index/home/facade/HelloController.java
@@ -1,18 +1,20 @@
 package com.index.home.facade;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author lushengkao vip8
  * 2018/11/1 14:36
  */
-@RestController
+@Controller
 public class HelloController {
 
     @RequestMapping("/")
-    public String index() {
-        return "hello world";
+    public String index(Model model) {
+        model.addAttribute("message","hello freemanager");
+        return "index";
     }
 
 }
---
+++ src/main/resources/templates/index.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Spring Boot Demo - FreeMarker</title>
+</head>
+<body>
+<h2>首页<h2>
+${message}
+</body>
+</html>
