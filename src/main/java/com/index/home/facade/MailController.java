d589cec0cd2c5307d98e8a41a749bc79bae22b1c
lucas2 <lukaozi@163.com>
2018-11-01 18:00:18 +0800

    首次创建

---
+++ src/main/java/com/index/home/facade/MailController.java
@@ -0,0 +1,11 @@
+package com.index.home.facade;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author lushengkao vip8
+ * 2018/11/1 17:59
+ */
+@Controller
+public class MailController {
+}
---
+++ src/main/java/com/index/home/service/MailService.java
@@ -0,0 +1,12 @@
+package com.index.home.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author lushengkao vip8
+ * 2018/11/1 18:00
+ */
+@Service
+public class MailService {
+
+}
